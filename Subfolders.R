11385a7e55dcd6affd6f15796ae71c2eca26e85e
Martinez Araneda <martie@uiowa.edu>
2025-10-02 13:18:58 -0500

    initial version.

---
+++ Subfolders.R
@@ -0,0 +1,14 @@
+# Create subfolder within the current working directory
+
+dir.create("Data")
+dir.create("R")
+dir.create("R/RTM")
+dir.create("Output")
+dir.create("Output/Data")
+dir.create("Output/Data/RTM")
+dir.create("Output/Plots")
+dir.create("Output/Plots/RTM")
+
+
+
+
