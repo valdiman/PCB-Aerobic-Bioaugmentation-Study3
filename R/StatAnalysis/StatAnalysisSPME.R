e3bbc4434d72cd3a1dce48e404abf82c8e056374
martie <andres-martinez@uiowa.edu>
2025-10-14 16:28:38 -0500

    update version.

--- R/StatAnalysis/StatAnalysisSPME.R
+++ R/StatAnalysis/StatAnalysisSPME.R
@@ -1,207 +1,102 @@
-# Script to analysis SPME data using ANOVA and Tukey's Honest test
+# Script to analysis SMPE data using ANOVA and Tukey's Honest test
 # for lab data from bioaugmentation experiments
 
 # Install Packages
-install.packages("lattice")
+install.packages("dplyr")
+install.packages("tidyr")
 
 # Load Libraries
-library(lattice) 
+library(dplyr)
+library(tidyr)
 
-# Read PUF data -----------------------------------------------------------
-PCB_data <- read.csv("Data/SPME_data_long.csv")
+# Read data ---------------------------------------------------------------
+obs.data <- read.csv("Data/uncoated_biochar_V2.csv")
 
-# See data
-print(PCB_data)
+# Format data -------------------------------------------------------------
+# Select SPME & time series data
+pcbi.spme <- obs.data %>%
+  filter(Sample_medium =="SPME", Experiment =="biochar_timeseries")
 
-# tPCB --------------------------------------------------------------------
-# Plot data
-xyplot(tPCB ~ factor(Time)|ID, group = Group,
-       data = PCB_data, auto.key = TRUE)
+pcbi.spme$Group_biochar <- paste(pcbi.spme$Group,
+                                 pcbi.spme$percent_biochar, sep = "_")
+pcbi.spme$Group_biochar <- factor(pcbi.spme$Group_biochar)
 
-xyplot(log10(tPCB) ~ factor(Time)|ID, group = Group,
-       data = PCB_data, auto.key = TRUE)
+# ANOVA -------------------------------------------------------------------
+pcb.cols <- grep("^PCB_", names(pcbi.spme), value = TRUE)
+time_points <- unique(pcbi.spme$time)
 
-# Stat Analysis (t test) --------------------------------------------------
-# two-sample t test
-# (1) AVL_S
-t.test(log10(tPCB) ~ Group, data = subset(PCB_data, Time == 3 & ID == "AVL_S"))
-t.test(log10(tPCB) ~ Group, data = subset(PCB_data, Time == 11 & ID == "AVL_S"))
-t.test(log10(tPCB) ~ Group, data = subset(PCB_data, Time == 35 & ID == "AVL_S"))
-# (2) AVL_NS
-t.test(log10(tPCB) ~ Group, data = subset(PCB_data, Time == 16 & ID == "AVL_NS"))
-t.test(log10(tPCB) ~ Group, data = subset(PCB_data, Time == 35 & ID == "AVL_NS"))
-t.test(log10(tPCB) ~ Group, data = subset(PCB_data, Time == 75 & ID == "AVL_NS"))
-# (3) NBH_NS
-t.test(log10(tPCB) ~ Group, data = subset(PCB_data, Time == 16 & ID == "NBH_NS"))
-t.test(log10(tPCB) ~ Group, data = subset(PCB_data, Time == 35 & ID == "NBH_NS"))
-t.test(log10(tPCB) ~ Group, data = subset(PCB_data, Time == 75 & ID == "NBH_NS"))
-# (4) AVL_S vs. AVL_NS
-# Time 16 days
-tmp.data = subset(PCB_data, Time == 16 & (ID == "AVL_S" | ID == "AVL_NS"))
-# Plot data
-xyplot(tPCB ~ factor(Time)|ID, group = Group, data = tmp.data,
-       auto.key = TRUE)
-xyplot(log10(tPCB) ~ factor(Time)|ID, group = Group, data = tmp.data,
-       auto.key = TRUE)
+all_pcb <- c(pcb.cols, "tPCB", "LCPCB")
+anova_pvalues <- data.frame(
+  Variable = character(),
+  Time = numeric(),
+  p_value = numeric()
+)
 
-# two-sample t test
-t.test(log10(tPCB) ~ ID, data = subset(tmp.data, Group == "Control"))
+for (time_pt in time_points) {
+  df_time <- pcbi.spme %>% filter(time == time_pt)
+  
+  for (col in all_pcb) {
+    fit <- aov(as.formula(paste(col, "~ Group_biochar")), data = df_time)
+    pval <- summary(fit)[[1]][["Pr(>F)"]][1]
+    
+    anova_pvalues <- rbind(anova_pvalues, data.frame(
+      Variable = col,
+      Time = time_pt,
+      p_value = pval
+    ))
+  }
+}
 
-# Time 35 days
-tmp.data = subset(PCB_data, Time == 35 & (ID == "AVL_S" | ID == "AVL_NS"))
-# Plot data
-xyplot(tPCB ~ factor(Time)|ID, group = Group, data = tmp.data,
-       auto.key = TRUE)
-xyplot(log10(tPCB) ~ factor(Time)|ID, group = Group, data = tmp.data,
-       auto.key = TRUE)
+anova_sig <- anova_pvalues %>%
+  filter(p_value < 0.05)
 
-# two-sample t test
-t.test(log10(tPCB) ~ ID, data = subset(tmp.data, Group == "Control"))
-t.test(log10(tPCB) ~ ID, data = subset(tmp.data, Group == "Treatment"))
+# Check
+print(anova_sig)
 
-# LCPCB --------------------------------------------------------------------
-# Plot data
-xyplot(LCPCB ~ factor(Time)|ID, group = Group,
-       data = PCB_data, auto.key = TRUE)
+# Export data
+write.csv(anova_sig, file = "Output/Data/Stat/ANOVA_SPME.csv")
 
-xyplot(log10(LCPCB) ~ factor(Time)|ID, group = Group,
-       data = PCB_data, auto.key = TRUE)
+# Tukey's test ------------------------------------------------------------
+# Initialize empty data frame
+tukey_sig_df <- data.frame()
 
-# Stat Analysis (t test) --------------------------------------------------
-# two-sample t test
-# (1) AVL_S
-t.test(log10(LCPCB) ~ Group, data = subset(PCB_data, Time == 3 & ID == "AVL_S"))
-t.test(log10(LCPCB) ~ Group, data = subset(PCB_data, Time == 11 & ID == "AVL_S"))
-t.test(log10(LCPCB) ~ Group, data = subset(PCB_data, Time == 16 & ID == "AVL_S"))
-t.test(log10(LCPCB) ~ Group, data = subset(PCB_data, Time == 35 & ID == "AVL_S"))
-# (2) AVL_NS
-t.test(log10(LCPCB) ~ Group, data = subset(PCB_data, Time == 16 & ID == "AVL_NS"))
-t.test(log10(LCPCB) ~ Group, data = subset(PCB_data, Time == 35 & ID == "AVL_NS"))
-t.test(log10(LCPCB) ~ Group, data = subset(PCB_data, Time == 75 & ID == "AVL_NS"))
-# (3) NBH_NS
-t.test(log10(LCPCB) ~ Group, data = subset(PCB_data, Time == 16 & ID == "NBH_NS"))
-t.test(log10(LCPCB) ~ Group, data = subset(PCB_data, Time == 35 & ID == "NBH_NS"))
-t.test(log10(LCPCB) ~ Group, data = subset(PCB_data, Time == 75 & ID == "NBH_NS"))
-# (4) AVL_S vs. AVL_NS
-# Time 16 days
-tmp.data = subset(PCB_data, Time == 16 & (ID == "AVL_S" | ID == "AVL_NS"))
-# Plot data
-xyplot(LCPCB ~ factor(Time)|ID, group = Group, data = tmp.data,
-       auto.key = TRUE)
-xyplot(log10(LCPCB) ~ factor(Time)|ID, group = Group, data = tmp.data,
-       auto.key = TRUE)
+# Loop through each significant ANOVA result
+for (i in 1:nrow(anova_sig)) {
+  var <- anova_sig$Variable[i]
+  t <- anova_sig$Time[i]
+  
+  df_time <- pcbi.spme %>% filter(time == t)
+  fit <- aov(as.formula(paste(var, "~ Group_biochar")), data = df_time)
+  tuk <- TukeyHSD(fit)
+  
+  df <- as.data.frame(tuk$Group_biochar) %>%
+    mutate(
+      Variable = var,
+      Time = t,
+      Comparison = rownames(.)
+    ) %>%
+    rename(p.adj = `p adj`) %>%
+    select(Variable, Time, Comparison, diff, lwr, upr, p.adj)
+  
+  # Split the comparison into two groups (e.g., "Control_0-Treatment_5")
+  df <- df %>%
+    tidyr::separate(Comparison, into = c("Group1", "Group2"), sep = "-") %>%
+    mutate(
+      Higher_Group = ifelse(diff > 0, Group1, Group2)  # which has higher mean
+    )
+  
+  # Keep only significant comparisons
+  df_sig <- df %>% filter(p.adj < 0.05)
+  
+  tukey_sig_df <- bind_rows(tukey_sig_df, df_sig)
+}
 
-# two-sample t test
-t.test(log10(LCPCB) ~ ID, data = subset(tmp.data, Group == "Control"))
-t.test(log10(LCPCB) ~ ID, data = subset(tmp.data, Group == "Treatment"))
+# Arrange and inspect
+tukey_sig_df <- tukey_sig_df %>%
+  arrange(Variable, Time, p.adj)
 
-# Time 35 days
-tmp.data = subset(PCB_data, Time == 35 & (ID == "AVL_S" | ID == "AVL_NS"))
-# Plot data
-xyplot(LCPCB ~ factor(Time)|ID, group = Group, data = tmp.data,
-       auto.key = TRUE)
-xyplot(log10(LCPCB) ~ factor(Time)|ID, group = Group, data = tmp.data,
-       auto.key = TRUE)
+print(tukey_sig_df)
 
-# two-sample t test
-t.test(log10(LCPCB) ~ ID, data = subset(tmp.data, Group == "Control"))
-t.test(log10(LCPCB) ~ ID, data = subset(tmp.data, Group == "Treatment"))
-
-# PCB 4 -------------------------------------------------------------------
-# Plot data
-xyplot(PCB_4 ~ factor(Time)|ID, group = Group,
-       data = PCB_data, auto.key = TRUE)
-
-xyplot(log10(PCB_4) ~ factor(Time)|ID, group = Group,
-       data = PCB_data, auto.key = TRUE)
-
-# Stat Analysis (t test) --------------------------------------------------
-# two-sample t test
-# (1) AVL_S
-t.test(log10(PCB_4) ~ Group, data = subset(PCB_data, Time == 3 & ID == "AVL_S"))
-t.test(log10(PCB_4) ~ Group, data = subset(PCB_data, Time == 11 & ID == "AVL_S"))
-t.test(log10(PCB_4) ~ Group, data = subset(PCB_data, Time == 35 & ID == "AVL_S"))
-# (2) AVL_NS
-t.test(log10(PCB_4) ~ Group, data = subset(PCB_data, Time == 16 & ID == "AVL_NS"))
-t.test(log10(PCB_4) ~ Group, data = subset(PCB_data, Time == 35 & ID == "AVL_NS"))
-t.test(log10(PCB_4) ~ Group, data = subset(PCB_data, Time == 75 & ID == "AVL_NS"))
-# (3) NBH_NS
-t.test(log10(PCB_4) ~ Group, data = subset(PCB_data, Time == 16 & ID == "NBH_NS"))
-t.test(log10(PCB_4) ~ Group, data = subset(PCB_data, Time == 35 & ID == "NBH_NS"))
-t.test(log10(PCB_4) ~ Group, data = subset(PCB_data, Time == 75 & ID == "NBH_NS"))
-
-# (4) AVL_S vs. AVL_NS
-# Time 16 days
-tmp.data = subset(PCB_data, Time == 16 & (ID == "AVL_S" | ID == "AVL_NS"))
-# Plot data
-xyplot(PCB_4 ~ factor(Time)|ID, group = Group, data = tmp.data,
-       auto.key = TRUE)
-xyplot(log10(PCB_4) ~ factor(Time)|ID, group = Group, data = tmp.data,
-       auto.key = TRUE)
-
-# two-sample t test
-t.test(log10(PCB_4) ~ ID, data = subset(tmp.data, Group == "Control"))
-
-# Time 35 days
-tmp.data = subset(PCB_data, Time == 35 & (ID == "AVL_S" | ID == "AVL_NS"))
-# Plot data
-xyplot(PCB_4 ~ factor(Time)|ID, group = Group, data = tmp.data,
-       auto.key = TRUE)
-xyplot(log10(PCB_4) ~ factor(Time)|ID, group = Group, data = tmp.data,
-       auto.key = TRUE)
-
-# two-sample t test
-t.test(log10(PCB_4) ~ ID, data = subset(tmp.data, Group == "Control"))
-t.test(log10(PCB_4) ~ ID, data = subset(tmp.data, Group == "Treatment"))
-
-# PCB 19 -------------------------------------------------------------------
-# Plot data
-xyplot(PCB_19 ~ factor(Time)|ID, group = Group,
-       data = PCB_data, auto.key = TRUE)
-
-xyplot(log10(PCB_19) ~ factor(Time)|ID, group = Group,
-       data = PCB_data, auto.key = TRUE)
-
-# Stat Analysis (t test) --------------------------------------------------
-# two-sample t test
-# (1) AVL_S
-t.test(log10(PCB_19) ~ Group, data = subset(PCB_data, Time == 3 & ID == "AVL_S"))
-t.test(log10(PCB_19) ~ Group, data = subset(PCB_data, Time == 11 & ID == "AVL_S"))
-t.test(log10(PCB_19) ~ Group, data = subset(PCB_data, Time == 35 & ID == "AVL_S"))
-# (2) AVL_NS
-t.test(log10(PCB_19) ~ Group, data = subset(PCB_data, Time == 16 & ID == "AVL_NS"))
-t.test(log10(PCB_19) ~ Group, data = subset(PCB_data, Time == 35 & ID == "AVL_NS"))
-t.test(log10(PCB_19) ~ Group, data = subset(PCB_data, Time == 75 & ID == "AVL_NS"))
-# (3) NBH_NS
-t.test(log10(PCB_19) ~ Group, data = subset(PCB_data, Time == 16 & ID == "NBH_NS"))
-t.test(log10(PCB_19) ~ Group, data = subset(PCB_data, Time == 35 & ID == "NBH_NS"))
-t.test(log10(PCB_19) ~ Group, data = subset(PCB_data, Time == 75 & ID == "NBH_NS"))
-
-# (4) AVL_S vs. AVL_NS
-# Time 16 days
-tmp.data = subset(PCB_data, Time == 16 & (ID == "AVL_S" | ID == "AVL_NS"))
-# Plot data
-xyplot(PCB_19 ~ factor(Time)|ID, group = Group, data = tmp.data,
-       auto.key = TRUE)
-xyplot(log10(PCB_19) ~ factor(Time)|ID, group = Group, data = tmp.data,
-       auto.key = TRUE)
-
-# two-sample t test
-t.test(log10(PCB_19) ~ ID, data = subset(tmp.data, Group == "Control"))
-
-# Time 35 days
-tmp.data = subset(PCB_data, Time == 35 & (ID == "AVL_S" | ID == "AVL_NS"))
-# Plot data
-xyplot(PCB_19 ~ factor(Time)|ID, group = Group, data = tmp.data,
-       auto.key = TRUE)
-xyplot(log10(PCB_19) ~ factor(Time)|ID, group = Group, data = tmp.data,
-       auto.key = TRUE)
-
-# two-sample t test
-t.test(log10(PCB_19) ~ ID, data = subset(tmp.data, Group == "Control"))
-t.test(log10(PCB_19) ~ ID, data = subset(tmp.data, Group == "Treatment"))
-
-
-
-
-
+# Export results
+write.csv(tukey_sig_df, file = "Output/Data/Stat/Tukey_SPME.csv",
+          row.names = FALSE)
